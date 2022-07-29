57474e5507e9ef6d72c8610b77a104eed67dce3f
tintin-2208 <mario.janjic@outlook.at>
2022-07-29 10:11:33 +0200

    Fertig

--- kontrolle der länge/Program.cs
+++ kontrolle der länge/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace kontrolle
 {
@@ -11,13 +13,31 @@ namespace kontrolle
     {
         static void Main(string[] args)
 
-        { 
+        {
             string zahl = "";
             string _kontrolle = "";
 
-            Console.WriteLine("Gib die nummer ein und die muss 13 stellen haben");
+            Console.WriteLine("Gib die nummer ein und die muss 13 stellen haben und beginnen mit 1101 ");
             zahl = Console.ReadLine();
-            if zahl = 
+            Console.WriteLine("du hast die nummer "+ (zahl)+ " eingegeben");
+
+            Regex check = new Regex("^(1101)[0-9]{9}$");
+            Match match = check.Match(zahl);
+            if (match.Success)
+            {
+                _kontrolle = zahl;
+                Console.WriteLine("Die nummer ist richtig " + (_kontrolle) + " und hat 13 stellen");
+            }
+            else
+            {
+                Console.WriteLine("Die nummer " + (zahl) + " ist nicht 13 Stellig !!! bitte noch");
+            }
+
+
+            Console.ReadLine();
+
+        }
+
 
 
 
