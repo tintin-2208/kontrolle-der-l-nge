78698a87a31cd658ac119e2b34ea0611fe37661b
tintin-2208 <mario.janjic@outlook.at>
2022-07-29 08:19:13 +0200

    Projektdateien hinzufügen.

---
+++ kontrolle der länge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kontrolle
+{
+    public class Program
+
+    {
+        static void Main(string[] args)
+
+        { 
+            string zahl = "";
+            string _kontrolle = "";
+
+            Console.WriteLine("Gib die nummer ein und die muss 13 stellen haben");
+            zahl = Console.ReadLine();
+            if zahl = 
+
+
+
+
+
+    }
+}
